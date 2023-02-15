2538fae1885e68ddd3bb8813e7b4d66267fcd42c
NetaShubin <shubinneta@gmail.com>
2023-02-15 14:39:02 -0800

    dani's demo

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -15,3 +15,19 @@ function getPrice() {
         cost.innerHTML = "$450";
     }
 }
+
+function reveal() {
+    var reveals = document.getElementsByClassName("reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("activate");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
