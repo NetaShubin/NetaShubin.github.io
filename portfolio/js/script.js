49e624e3d4a9d7794658068e6d2c859d36901b86
NetaShubin <shubinneta@gmail.com>
2023-03-03 12:23:50 -0800

    back to top button

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -31,3 +31,30 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+// Back to top button- https://mdbootstrap.com/snippets/standard/mdbootstrap/2964350#js-tab-view
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    //https://stackoverflow.com/questions/15935318/smooth-scroll-to-top
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
